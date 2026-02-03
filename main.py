ea10ea7e82b78a7a4eef01afecf1bdd78d6b99f4
iamminhquan <minhquan542005@gmail.com>
2026-02-04 00:35:41 +0700

    feat: add basic implementation about interchange sort algorithm

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import math
+import os
+import sys
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print_hi('Michael')
+def is_increment(collection: list) -> bool:
+    length: int = len(collection)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for i in range(length - 1):
+        if collection[i] > collection[i + 1]:
+            return False
+
+    return True
+
+
+def interchange_sort(collection: list) -> None:
+    length: int = len(collection)
+
+    for i in range(length):
+        for j in range(length):
+            if collection[i] < collection[j]:
+                # Swap two elements.
+                collection[i], collection[j] = collection[j], collection[i]
+
+
+def main() -> None:
+    # Init the collection.
+    collection: list = [10, 9, 8, 7, 1, 2, 3, 4, 5, 6]
+    print(collection)
+
+    # Sorting the collection to ensure the collection is increment.
+    interchange_sort(collection)
+
+    # Print the collection after sorted.
+    print(collection)
+
+    # Check if the collection is not sorted, then print "NO" to the terminal.
+    if is_increment(collection):
+        print("Yes, the collection is already sorted.")
+    else:
+        print("No, the collection is not already sorted.")
+
+
+if __name__ == "__main__":
+    main()
