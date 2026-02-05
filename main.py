d31889c3cb3aa09a73995e2c2682cdf61555dc2d
iamminhquan <minhquan542005@gmail.com>
2026-02-05 16:58:25 +0700

    updated main

--- main.py
+++ main.py
@@ -1,44 +1,10 @@
-import math
-import os
-import sys
-
-
-def is_increment(collection: list) -> bool:
-    length: int = len(collection)
-
-    for i in range(length - 1):
-        if collection[i] > collection[i + 1]:
-            return False
-
-    return True
-
-
-def interchange_sort(collection: list) -> None:
-    length: int = len(collection)
-
-    for i in range(length):
-        for j in range(length):
-            if collection[i] < collection[j]:
-                # Swap two elements.
-                collection[i], collection[j] = collection[j], collection[i]
+def print_hi(full_name: str) -> None:
+    print(f"Hello, {full_name}!")
 
 
 def main() -> None:
-    # Init the collection.
-    collection: list = [10, 9, 8, 7, 1, 2, 3, 4, 5, 6]
-    print(collection)
-
-    # Sorting the collection to ensure the collection is increment.
-    interchange_sort(collection)
-
-    # Print the collection after sorted.
-    print(collection)
-
-    # Check if the collection is not sorted, then print "NO" to the terminal.
-    if is_increment(collection):
-        print("Yes, the collection is already sorted.")
-    else:
-        print("No, the collection is not already sorted.")
+    print_hi("World")
+    print_hi("Minh Quan")
 
 
 if __name__ == "__main__":
