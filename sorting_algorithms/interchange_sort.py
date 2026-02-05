f35e679af19f5b6849b41bebeb94db18706afea5
iamminhquan <minhquan542005@gmail.com>
2026-02-05 22:11:01 +0700

    docs: enhance docstring for interchange_sort function with detailed explanation

--- sorting_algorithms/interchange_sort.py
+++ sorting_algorithms/interchange_sort.py
@@ -5,6 +5,27 @@ MAX: int = 2000
 
 
 def interchange_sort(collection: list[int]) -> None:
+    """
+    Sort a list of integers in ascending order using the interchange sort algorithm.
+
+    This algorithm compares each element with all subsequent elements and swaps
+    them if they are in the wrong order. Also known as naive sort or exchange sort.
+
+    :param collection: The list of integers to be sorted.
+    :type collection: list[int]
+    :return: None. The list is sorted in-place.
+    :rtype: None
+
+    :Example:
+
+    >>> arr = [64, 34, 25, 12, 22, 11, 90]
+    >>> interchange_sort(arr)
+    >>> arr
+    [11, 12, 22, 25, 34, 64, 90]
+
+    :Time Complexity: O(n^2) in all cases (best, average, worst).
+    :Space Complexity: O(1) - sorts in-place.
+    """
     for i in range(len(collection) - 1):
         for j in range(i + 1, len(collection)):
             if collection[i] > collection[j]:
